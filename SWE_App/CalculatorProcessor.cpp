0811018a63abaf7e38639121744e453edf638ce1
jmonraz <jmonraz@student.fullsail.edu>
2022-10-14 10:55:56 -0700

    Added algorithm to generate tokens based on a passed string

--- SWE_App/CalculatorProcessor.cpp
+++ SWE_App/CalculatorProcessor.cpp
@@ -1,4 +1,10 @@
 #include "CalculatorProcessor.h"
+#include <stack>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <sstream>
 
 CalculatorProcessor* CalculatorProcessor::_calculatorProcessor = nullptr;
 
@@ -9,4 +15,47 @@ CalculatorProcessor* CalculatorProcessor::GetInstance()
 		_calculatorProcessor = new CalculatorProcessor();
 	}
 	return _calculatorProcessor;
+}
+
+void CalculatorProcessor::Calculate(std::string calc)
+{
+	std::string str = ConverString(calc);
+
+	GenerateTokens(str);
+
+
+
+}
+
+void CalculatorProcessor::GenerateTokens(std::string calc)
+{
+	std::istringstream iss(calc);
+
+	std::string s;
+	while (iss >> s)
+	{
+		tokens.push_back(s);
+	}
+}
+
+std::string CalculatorProcessor::ConverString(std::string str)
+{
+	std::string temp = str;
+
+	for (int i = 0; i < temp.size(); i++)
+	{
+		if (temp[i] == '+' ||
+			temp[i] == '*' ||
+			temp[i] == '(' ||
+			temp[i] == ')' ||
+			temp[i] == '/' ||
+			temp[i] == '%' ||
+			temp[i] == '-')
+		{
+			temp.insert(i, 1, ' ');
+			temp.insert(i + 2, 1, ' ');
+			i += 2;
+		}
+	}
+	return temp;
 }
--- SWE_App/CalculatorProcessor.h
+++ SWE_App/CalculatorProcessor.h
@@ -1,12 +1,27 @@
 #pragma once
+#include <stack>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+
 class CalculatorProcessor
 {
 private:
+	std::string input = "";
+	std::string out = "";
+	std::queue<std::string> output;
+	std::stack<std::string> operators;
+	std::vector<std::string> tokens;
+
 	static CalculatorProcessor* _calculatorProcessor;
 	CalculatorProcessor() {}
 public:
 	CalculatorProcessor(CalculatorProcessor& other) = delete;
 	CalculatorProcessor& operator=(CalculatorProcessor& other) = delete;
 	static CalculatorProcessor* GetInstance();
+	void Calculate(std::string calc);
+	void GenerateTokens(std::string calc);
+	std::string ConverString(std::string str);
 };
 
