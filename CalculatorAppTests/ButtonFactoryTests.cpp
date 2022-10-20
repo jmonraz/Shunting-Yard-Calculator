91884cd006182cf2aabf7dc4fcd14d5e5cac5730
jmonraz <jmonraz@student.fullsail.edu>
2022-10-20 15:49:33 -0700

    All 20 tests succeded.

--- CalculatorAppTests/ButtonFactoryTests.cpp
+++ CalculatorAppTests/ButtonFactoryTests.cpp
@@ -36,33 +36,65 @@ namespace ButtonFactoryTests
 			Assert::AreNotEqual(compare, str);
 
 		}
-		TEST_METHOD(test4)
+		TEST_METHOD(CrateNumberButton)
 		{
+			wxButton* nine = ButtonFactory::CreateButton(win, '9', "9", wxPoint(10, 10), wxSize(20, 20), font);
 
+			int id = nine->GetId();
+
+			Assert::AreEqual(57, id);
 		}
-		TEST_METHOD(test5)
+		TEST_METHOD(CreatePlusButton)
 		{
+			wxButton* plus = ButtonFactory::CreateButton(win, '+', "+", wxPoint(10, 10), wxSize(20, 20), font);
 
+			std::string str = plus->GetLabel();
+
+			Assert::IsTrue(str == "+");
 		}
-		TEST_METHOD(test6)
+		TEST_METHOD(CreateLeftParenthesis)
 		{
+			wxButton* leftParenthesis = ButtonFactory::CreateButton(win, '(', "(", wxPoint(10, 10), wxSize(100, 10), font);
 
+			std::string str = leftParenthesis->GetLabel();
+			std::string compare = "(";
+
+			Assert::AreEqual(compare, str);
 		}
-		TEST_METHOD(test7)
+		TEST_METHOD(CreateModButton)
 		{
+			wxButton* modButton = ButtonFactory::CreateButton(win, 89, "%", wxPoint(10, 10), wxSize(100, 10), font);
 
+			std::string str = modButton->GetLabel();
+			std::string compare = "%";
+
+			Assert::AreEqual(compare, str);
 		}
-		TEST_METHOD(test8)
+		TEST_METHOD(CreateZeroButton)
 		{
+			wxButton* zeroButton = ButtonFactory::CreateButton(win, 400, "0", wxPoint(10, 10), wxSize(100, 10), font);
 
+			int id = zeroButton->GetId();
+
+			Assert::AreEqual(400, id);
 		}
-		TEST_METHOD(test9)
+		TEST_METHOD(CreateDivisionButton)
 		{
+			wxButton* divisionButton = ButtonFactory::CreateButton(win, '/', "/", wxPoint(10, 10), wxSize(100, 10), font);
 
+			std::string str = divisionButton->GetLabel();
+			std::string compare = "/";
+
+			Assert::AreEqual(compare, str);
 		}
-		TEST_METHOD(test10)
+		TEST_METHOD(CreateMultiplicationButton)
 		{
+			wxButton* multiplicationButton = ButtonFactory::CreateButton(win, 400, "*", wxPoint(10, 10), wxSize(100, 10), font);
 
+			std::string str = multiplicationButton->GetLabel();
+			std::string compare = "+";
+
+			Assert::IsFalse(compare == str);
 		}
 	};
 }
